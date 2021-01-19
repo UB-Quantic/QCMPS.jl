67cf05785325cedca75cffdf94092848cd475c7f
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2021-01-19 10:53:16 +0100

    Fix apply! function

--- src/MPS.jl
+++ src/MPS.jl
@@ -7,8 +7,5 @@ end
 # TODO how do yo initialize an MPS to |0..0⟩?
 MPS(n::Integer,χ::Integer) = MPS([complex(rand(χ,χ,2)) for _ in 1:n])
 
-function apply!(ψ::MPS, U::Array{F,2}, target) where F
-    A = ψ.A[target]
-    @tensor A[i,j,k] = A[i,j,l] * U[l,k]
-end
+apply!(ψ::MPS, U::Array{N,2}, target::Integer) where N <: Number = @tensor ψ.A[target][i,j,k] := ψ.A[target][i,j,l] * U[l,k]
 
