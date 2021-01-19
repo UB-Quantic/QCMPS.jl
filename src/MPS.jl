7ffe3360ba2229ffe032085bbc17656cea6d86bb
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2021-01-19 10:48:56 +0100

    Refactor MPS constructor

--- src/MPS.jl
+++ src/MPS.jl
@@ -1,12 +1,11 @@
 using TensorOperations
 
 struct MPS
-    χ::Unsigned
     A::Vector{Array{Complex{AbstractFloat},3}}
 end
 
 # TODO how do yo initialize an MPS to |0..0⟩?
-MPS(n::Integer,χ::Integer) = MPS(χ, [complex(zeros(χ,χ,2)) for _ in 1:n])
+MPS(n::Integer,χ::Integer) = MPS([complex(rand(χ,χ,2)) for _ in 1:n])
 
 function apply!(ψ::MPS, U::Array{F,2}, target) where F
     A = ψ.A[target]
