71ab85d72636c132fb688f442de3c0d91da839ef
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2021-01-29 14:05:26 +0100

    Refactor apply! methods to method on MPS

--- src/LocalMPS.jl
+++ src/LocalMPS.jl
@@ -5,15 +5,3 @@ end
 
 LocalMPS(χ,n::Integer) = LocalMPS(Float32, χ, n)
 LocalMPS(::Type{T},χ,n) where T = LocalMPS(χ, [MPT(T, χ) for _ in 1:n])
-
-apply!(ψ::LocalMPS, H::Array{N,2}, target::Integer) where N <: Number = apply!(ψ.A[target], H)
-
-apply!(ψ::LocalMPS, H::Array{N,4}, targets::Tuple{Integer,Integer}) where N <: Number = begin
-	# TODO order inverted operators
-    @assert (targets[1] == targets[2] - 1) "Only 1-local operators are allowed"
-
-	ψ₁ = ψ.A[targets[1]]
-	ψ₂ = ψ.A[targets[2]]
-
-	apply!(ψ₁, ψ₂, H)
-end
--- src/MPS.jl
+++ src/MPS.jl
@@ -10,4 +10,5 @@ include("DistributedMPS.jl")
 
 MPS(χ::Integer, n::Integer) = LocalMPS(χ)
 
+apply!(Ψ::MPS, H::Array{N,2}, i::Integer) where N <: Number = apply!(Ψ.A[i], H)
 apply_all!(Ψ::MPS, H::Array{N,2}) where N <: Number = foreach(i -> apply!(Ψ, H, i), 1:length(Ψ.A))
