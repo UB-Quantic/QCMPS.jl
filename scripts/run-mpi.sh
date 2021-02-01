346c6e74cf8257024ffdefc80752a01638499194
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2021-02-01 16:00:26 +0100

    Fix run-mpi.sh

--- scripts/run-mpi.sh
+++ scripts/run-mpi.sh
@@ -6,20 +6,20 @@ while [[ $# -gt 0 ]]
 do
 	case $1 in
 		--project)
-			export PROJECT="--project=$2"
+			PROJECT="--project=$2"
 			shift
 			;;
 		-np)
-			export NP="$2"
+			NP="$2"
 			shift
 			;;
 		--extrae)
 			export EXTRAE_CONFIG_FILE=extrae.xml
 			export LD_PRELOAD=${EXTRAE_HOME}/lib/libompitrace.so
-			shift
 			;;
 		*)
 			SCRIPT+="$1 "
+			;;
 	esac
 
 	shift
