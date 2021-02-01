4791c847ff43637fd18117bc0890bcae4046430a
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2021-02-01 14:49:50 +0100

    Fix config-mpi.sh script

--- scripts/config-mpi.sh
+++ scripts/config-mpi.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+PROJECT=
 VERBOSE=false
-while :; do
+MPIEXECJL_INSTALL=false
+
+while [[ $# -gt 0 ]]
+do
     case $1 in
+		--project)
+			PROJECT="--project"
+			shift
+			;;
 		--bin)
 			export JULIA_MPI_BINARY="$2"
 			shift
@@ -34,14 +42,17 @@ while :; do
     shift
 done
 
-if ["$VERBOSE" = false]; then
+if [ "$VERBOSE" = true ]; then
 	echo "JULIA_MPI_BINARY = $JULIA_MPI_BINARY"
 	echo "JULIA_MPI_PATH = $JULIA_MPI_PATH"
 	echo "JULIA_MPI_LIBRARY = $JULIA_MPI_LIBRARY"
 	echo "JULIA_MPI_ABI = $JULIA_MPI_ABI"
 	echo "JULIA_MPIEXEC = $JULIA_MPIEXEC"
-	echo "Install mpiexecjl? = $MPIEXECJL_INSTALL"
+	echo "Install mpiexecjl? $MPIEXECJL_INSTALL"
 fi
 
-julia -e "using Pkg; Pkg.build("MPI"; verbose=true)"
-if ["$MPIEXECJL_INSTALL" = true]; then julia -e "using MPI; MPI.install_mpiexecjl()" fi
+julia $PROJECT -e 'using Pkg; Pkg.build("MPI"; verbose=true)'
+
+if [ "$MPIEXECJL_INSTALL" = true ]; then
+	julia -e "using MPI; MPI.install_mpiexecjl()"
+fi
