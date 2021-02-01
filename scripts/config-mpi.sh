301c453c9d4b41919904053fcfe43f61744284a8
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2021-02-01 14:43:21 +0100

    Add verbose flag to config-mpi.sh

--- scripts/config-mpi.sh
+++ scripts/config-mpi.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+VERBOSE=false
 while :; do
     case $1 in
 		--bin)
@@ -24,10 +25,23 @@ while :; do
 			;;
 		--install)
 			MPIEXECJL_INSTALL=true
+			;;
+		-v|--verbose)
+			VERBOSE=true
+			;;
     esac
 
     shift
 done
+
+if ["$VERBOSE" = false]; then
+	echo "JULIA_MPI_BINARY = $JULIA_MPI_BINARY"
+	echo "JULIA_MPI_PATH = $JULIA_MPI_PATH"
+	echo "JULIA_MPI_LIBRARY = $JULIA_MPI_LIBRARY"
+	echo "JULIA_MPI_ABI = $JULIA_MPI_ABI"
+	echo "JULIA_MPIEXEC = $JULIA_MPIEXEC"
+	echo "Install mpiexecjl? = $MPIEXECJL_INSTALL"
+fi
 
 julia -e "using Pkg; Pkg.build("MPI"; verbose=true)"
 if ["$MPIEXECJL_INSTALL" = true]; then julia -e "using MPI; MPI.install_mpiexecjl()" fi
